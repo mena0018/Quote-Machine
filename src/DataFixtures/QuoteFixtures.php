bc63bb9e9c150e120cbc84fa0827179e8b469281
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-10-10 19:06:40 +0200

    Ajout de fixtures réaliste pour les citations

--- src/DataFixtures/QuoteFixtures.php
+++ src/DataFixtures/QuoteFixtures.php
@@ -10,6 +10,31 @@ class QuoteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        QuoteFactory::createOne([
+            'content' => 'Qu\'est-ce que vous voulez-vous insinuyer Sire ?',
+            'meta' => 'Roparzh, Kaamelott, Livre III, 74 : Saponides et detergents',
+        ]);
+
+        QuoteFactory::createOne([
+            'content' => 'Sire, Sire ! On en a gros !',
+            'meta' => 'Perceval, Kaamelott, Livre II, Les Exploités',
+        ]);
+
+        QuoteFactory::createOne([
+            'content' => 'Mais évidemment c\'est sans alcool !',
+            'meta' => 'Merlin, Kaamelott, Livre II, 4 : Le rassemblement du corbeau',
+        ]);
+
+        QuoteFactory::createOne([
+            'content' => 'Quand on veut être sûr de son coup, Seigneur Dagonet… on plante des navets. On ne pratique pas le putsch.',
+            'meta' => 'Loth, Kaamelott, Livre V, Les Repentants',
+        ]);
+
+        QuoteFactory::createOne([
+            'content' => 'Vous savez c\'que c\'est, mon problème ? Trop gentil.',
+            'meta' => 'Léodagan, Kaamelott, Livre II, Le complot',
+        ]);
+
         QuoteFactory::createMany(5);
     }
 }
