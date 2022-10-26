568a9fb4e7fef722dc091b279a5a4d454850ec9b
mena0018 <rabie.menad@etudiant.univ-reims.r>
2022-10-26 11:59:23 +0200

    Ajout d'une categorie pour chaque citation généré par les fixtures

--- src/DataFixtures/QuoteFixtures.php
+++ src/DataFixtures/QuoteFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use App\Factory\QuoteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -10,31 +11,40 @@ class QuoteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $kaamelottCategory = CategoryFactory::find(['name' => 'Kaamelott']);
+
         QuoteFactory::createOne([
             'content' => 'Qu\'est-ce que vous voulez-vous insinuyer Sire ?',
             'meta' => 'Roparzh, Kaamelott, Livre III, 74 : Saponides et detergents',
+            'category' => $kaamelottCategory
         ]);
 
         QuoteFactory::createOne([
             'content' => 'Sire, Sire ! On en a gros !',
             'meta' => 'Perceval, Kaamelott, Livre II, Les Exploités',
+            'category' => $kaamelottCategory
         ]);
 
         QuoteFactory::createOne([
             'content' => 'Mais évidemment c\'est sans alcool !',
             'meta' => 'Merlin, Kaamelott, Livre II, 4 : Le rassemblement du corbeau',
+            'category' => $kaamelottCategory
         ]);
 
         QuoteFactory::createOne([
             'content' => 'Quand on veut être sûr de son coup, Seigneur Dagonet… on plante des navets. On ne pratique pas le putsch.',
             'meta' => 'Loth, Kaamelott, Livre V, Les Repentants',
+            'category' => $kaamelottCategory
         ]);
 
         QuoteFactory::createOne([
             'content' => 'Vous savez c\'que c\'est, mon problème ? Trop gentil.',
             'meta' => 'Léodagan, Kaamelott, Livre II, Le complot',
+            'category' => $kaamelottCategory
         ]);
 
-        QuoteFactory::createMany(5);
+        QuoteFactory::createMany(5, function() {
+            return ['category' => CategoryFactory::random()];
+        });
     }
 }
