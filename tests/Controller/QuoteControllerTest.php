d131f95c3b8f875153aa9c9dd969bb0df792ea1f
mena0018 <rabie.menad@etudiant.univ-reims.r>
2022-12-07 11:59:51 +0100

    Ajout du test fonctionnel permettant d'ajouter une citation

--- tests/Controller/QuoteControllerTest.php
+++ tests/Controller/QuoteControllerTest.php
@@ -2,10 +2,18 @@
 
 namespace App\Tests\Controller;
 
+use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Foundry\Test\Factories;
 
 class QuoteControllerTest extends WebTestCase
 {
+    // Permet à foundry de fonctionner dans les tests
+    use Factories;
+
+    private string $content = 'Ceci est une citation de test';
+    private string $meta = 'PhpUnit';
+
     public function testHomePage(): void
     {
         $client = static::createClient();
@@ -13,5 +21,27 @@ class QuoteControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Liste des citations');
+    }
+
+    public function testCreationQuote(): void
+    {
+        $client = static::createClient();
+        $user = UserFactory::createOne()->object();
+
+        // simule que $user est authentifié
+        $client->loginUser($user);
+
+        $client->request('POST', '/quote/new');
+        $client->submitForm('Sauvegarder', [
+            'quote[content]' => $this->content,
+            'quote[meta]' => $this->meta,
+        ]);
+
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $this->assertRouteSame('quote_index');
+        $this->assertSelectorTextContains('blockquote', $this->content);
+        $this->assertSelectorTextContains('cite', $this->meta);
     }
 }
