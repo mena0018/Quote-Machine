8ec0ab018ee58a350f48dfc56ff3e6f9d6a145f8
mena0018 <rabie.menad@etudiant.univ-reims.r>
2022-12-14 09:27:21 +0100

    Ajout du test de création d'une catégorie

--- tests/Functional/Category/CategoryControllerTest.php
+++ tests/Functional/Category/CategoryControllerTest.php
@@ -4,19 +4,43 @@ namespace App\Tests\Functional\Category;
 
 use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Foundry\Test\Factories;
 
 class CategoryControllerTest extends WebTestCase
 {
+    use Factories;
+
+    private const NAME = 'Kaamelott-Test';
+
     public function testCategoryIndexPage(): void
     {
         $client = static::createClient();
         $user = UserFactory::createOne()->object();
         $client->loginUser($user);
 
-        $client->request('GET', '/category');
+        $client->request('GET', '/category/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Liste des catégories');
+    }
+
+    public function testCreationCategory(): void
+    {
+        $client = static::createClient();
+        $user = UserFactory::createOne([
+            'roles' => ['ROLE_ADMIN'],
+        ])->object();
+        $client->loginUser($user);
+
+        $client->request('GET', '/category/new');
+        $client->submitForm('Sauvegarder', [
+            'category[name]' => self::NAME,
+        ]);
 
         $client->followRedirect();
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Liste des catégories');
+
+        $this->assertRouteSame('app_category_index');
+        $this->assertSelectorTextContains('body', self::NAME);
     }
 }
