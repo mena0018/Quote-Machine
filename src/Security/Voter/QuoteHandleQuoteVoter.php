c0c3ec58033e91fed2578ff4e45038d2bc1b0ba7
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2023-01-01 21:16:54 +0100

    Création du Voter permettant de restreindre la modification et la suppression d'une citation à son auteur

---
+++ src/Security/Voter/QuoteHandleQuoteVoter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Quote;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class QuoteHandleQuoteVoter extends Voter
+{
+    public const EDIT = 'EDIT';
+    public const DELETE = 'DELETE';
+
+    public function __construct(private Security $security)
+    {
+    }
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        return in_array($attribute, [self::EDIT, self::DELETE])
+            && $subject instanceof \App\Entity\Quote;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        /** @var Quote $quote */
+        $quote = $subject;
+
+        return match ($attribute) {
+            self::EDIT, self::DELETE => $quote->getAuthor()->getId() === $user->getId(),
+            default => false,
+        };
+    }
+}
