499beefbba1a049a602bfe9bf232e3833ea6671b
mena0018 <rabie.menad@etudiant.univ-reims.r>
2023-01-04 09:31:49 +0100

    Création d'une page de profil utilisateur

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/profile/{id}', name: 'app_profile', methods: ['GET'])]
+    public function profile(User $user): Response
+    {
+        return $this->render('user/profile.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
