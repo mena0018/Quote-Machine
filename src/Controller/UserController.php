bc25a2c803328ddd1d7c0b8c03c862eb47178a82
mena0018 <rabie.menad@etudiant.univ-reims.r>
2023-01-04 10:23:41 +0100

    Ajout d'une liste des 5 dernières citations de l'utilisateur sur la page de profil.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     #[Route('/profile/{id}', name: 'app_profile', methods: ['GET'])]
-    public function profile(User $user): Response
+    public function profile(User $user, EntityManagerInterface $entityManager): Response
     {
+        $query = $entityManager->createQuery(
+            'SELECT c
+             FROM App\Entity\Quote c
+             WHERE c.author = :author
+             ORDER BY c.createdAt DESC'
+        )
+            ->setParameter('author', $user)
+            ->setMaxResults(5);
+
         return $this->render('user/profile.html.twig', [
             'user' => $user,
+            'quotes' => $query->getResult(),
         ]);
     }
 }
