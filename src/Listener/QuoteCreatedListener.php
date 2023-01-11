f85c85d4b78d56bd23ec666d0ab99ecec4d4d524
mena0018 <rabie.menad@etudiant.univ-reims.r>
2023-01-11 10:21:21 +0100

    Ajout de l'expérience à l'utilisateur en fonction des cas de figure

---
+++ src/Listener/QuoteCreatedListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listener;
+
+use App\Event\QuoteCreatedEvent;
+use App\Repository\QuoteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+#[AsEventListener]
+class QuoteCreatedListener
+{
+    public function __construct(
+    private EntityManagerInterface $entityManager,
+    private QuoteRepository $quoteRepository
+  ) {
+    }
+
+    public function __invoke(QuoteCreatedEvent $event): void
+    {
+        $quote = $event->getQuote();
+        $author = $quote->getAuthor();
+        $experience = $author->getExperience();
+
+        $userNeverPost = 1 === $this->quoteRepository->count([
+          'category' => $quote->getCategory(),
+          'author' => $author,
+        ]);
+
+        $userNeverPost ? $author->setExperience($experience + 120) : $author->setExperience($experience + 100);
+
+        $this->entityManager->persist($quote);
+        $this->entityManager->flush();
+    }
+}
