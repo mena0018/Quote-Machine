25bdfcbf37fb58c895269c87d3f70b7c67873e49
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2023-01-31 21:29:09 +0100

    Ajout de la méthode computeExperienceNeededForLevel

--- src/Utill/GamificationEngine.php
+++ src/Utill/GamificationEngine.php
@@ -2,22 +2,10 @@
 
 namespace App\Util;
 
-use App\Entity\User;
-
 class GamificationEngine
 {
-  public static function computeLevelForUser(User $user): int
-  {
-    return 5;
-  }
-
-  public static function computeExperienceNeededForLevel(int $level): int
-  {
-    return 5;
-  }
-
-  public static function computeLevelCompletionForUser(User $user): int|float
-  {
-    return 5;
-  }
+    public static function computeExperienceNeededForLevel(int $level): int
+    {
+        return $level * ($level - 1) * 100 / 2;
+    }
 }
